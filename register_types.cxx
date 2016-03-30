b8495f4a289af541510efa0f93b6f023e154a7dd
Kazuo256 <kazuo256@gmail.com>
2016-03-30 13:06:18 -0300

    Env setup
    Testing an empty class

--- register_types.cxx
+++ register_types.cxx
@@ -2,8 +2,20 @@
 #include "register_types.h"
 #include "object_type_db.h"
 
+namespace {
+
+class ODAModule : public Reference {
+  OBJ_TYPE(ODAModule, Reference);
+  protected:
+    static void _bind_methods () {
+      // nothing;
+    }
+};
+
+} // unnamed namespace
+
 void register_openda_types () {
-  // nothing for now
+  ObjectTypeDB::register_type<ODAModule>();
 }
 
 void unregister_openda_types () {
