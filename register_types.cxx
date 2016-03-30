6fc0c4755154d6339deb7de953b8ee48bd847a44
Kazuo256 <kazuo256@gmail.com>
2016-03-30 12:59:43 -0300

    Basic Godot module template

---
+++ register_types.cxx
@@ -0,0 +1,12 @@
+
+#include "register_types.h"
+#include "object_type_db.h"
+
+void register_openda_types () {
+  // nothing for now
+}
+
+void unregister_openda_types () {
+  // nothing at all?
+}
+
---
+++ register_types.h
@@ -0,0 +1,9 @@
+
+#ifndef ODA_GODOT_REGISTER_TYPES_H_
+#define ODA_GODOT_REGISTER_TYPES_H_
+
+void register_openda_types ();
+void unregister_openda_types ();
+
+#endif // ODA_GODOT_REGISTER_TYPES_H_
+
